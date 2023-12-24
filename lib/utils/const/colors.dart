76c4a2879d701bb320612df8aa094112cd6adb08
Gajera Naman <125533942+NamanGajera@users.noreply.github.com>
2023-12-25 05:04:52 +0530

    Update colors.dart

--- lib/utils/const/colors.dart
+++ lib/utils/const/colors.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 class Ncolor {
+  //// hiii
   static const Color lightblue = Color(0xff93c0d3);
   static const Color darkblue1 = Color(0xff164863);
   static const Color darkblue2 = Color(0xff427D9D);
